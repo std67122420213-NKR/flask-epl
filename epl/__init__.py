04b7f5df898264d7e130cfb9e8165bb4c8cf4edf
std67122420213-NKR <std.67122420213@ubru.ac.th>
2026-02-10 12:15:14 +0700

    edit

--- epl/__init__.py
+++ epl/__init__.py
@@ -9,7 +9,7 @@ def create_app():
   app = Flask(__name__)
   app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/epl_s02_db'
   # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/epl_s02_db'
-  app.secret_key = b'hguyfdrerdfguhiophgytrt'
+  app.secret_key = b'Pineapple'
   
   db.init_app(app)
   migrate.init_app(app, db)

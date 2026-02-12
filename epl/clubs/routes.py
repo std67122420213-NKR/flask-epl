24bdaf008dc73a9ad0d840d810e0fe9a84bb82d7
std67122420213-NKR <std.67122420213@ubru.ac.th>
2026-02-12 12:56:58 +0700

    edit run

--- epl/clubs/routes.py
+++ epl/clubs/routes.py
@@ -1,17 +1,18 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from epl.extensions import db
 from epl.models import Club
+from epl.extensions import db
 
-club_bp = Blueprint('clubs', __name__, template_folder='templates')
+clubs_bp = Blueprint('clubs', __name__, template_folder='templates')
 
-@club_bp.route('/')
+@clubs_bp.route('/')
 def index():
-  clubs = db.session.scalars(db.select(Club)).all()
-  return render_template('clubs/index.html',
-                         title='Clubs Page',
+  query = db.select(Club)
+  clubs = db.session.scalars(query).all()
+  return render_template('clubs/index.html', 
+                         title='Clubs Page', 
                          clubs=clubs)
 
-@club_bp.route('/new', methods=['GET', 'POST'])
+@clubs_bp.route('/clubs/new', methods=['GET', 'POST'])
 def new_club():
   if request.method == 'POST':
     name = request.form['name']
@@ -22,29 +23,30 @@ def new_club():
     club = Club(name=name, stadium=stadium, year=year, logo=logo)
     db.session.add(club)
     db.session.commit()
+
     flash('add new club successfully', 'success')
     return redirect(url_for('clubs.index'))
   
   return render_template('clubs/new_club.html',
                          title='New Club Page')
 
-@club_bp.route('/search', methods=['GET', 'POST'])
+@clubs_bp.route('/clubs/search', methods=['GET', 'POST'])
 def search_club():
   if request.method == 'POST':
     club_name = request.form['club_name']
     clubs = db.session.scalars(db.select(Club).where(Club.name.like(f'%{club_name}%'))).all()
     return render_template('clubs/search_club.html',
-                          title='Search Club Page',
-                          clubs=clubs)
+                           title='Search Club Page',
+                           clubs=clubs)
   
-@club_bp.route('/<int:id>/info')
+@clubs_bp.route('/clubs/<int:id>/info')
 def info_club(id):
   club = db.session.get(Club, id)
   return render_template('clubs/info_club.html',
-                         title='Info Club Page',
+                         title='Club Info Page',
                          club=club)
 
-@club_bp.route('/<int:id>/update', methods=['GET','POST'])
+@clubs_bp.route('/clubs/<int:id>/update', methods=['GET', 'POST'])
 def update_club(id):
   club = db.session.get(Club, id)
   if request.method == 'POST':
--- epl/players/routes.py
+++ epl/players/routes.py
@@ -1,40 +1,43 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, request, url_for, redirect, flash, render_template
 from epl.extensions import db
 from epl.models import Club, Player
 
-player_bp = Blueprint('players', __name__, template_folder='templates')
+players_bp = Blueprint('players', __name__, template_folder='templates')
 
-@player_bp.route('/')
+@players_bp.route('/')
 def index():
-  players = db.session.scalars(db.select(Player)).all()
+  query = db.select(Player)
+  players = db.session.scalars(query).all()
   return render_template('players/index.html',
                          title='Players Page',
                          players=players)
 
-@player_bp.route('/new', methods=['GET', 'POST'])
+@players_bp.route('/players/new', methods=['GET', 'POST'])
 def new_player():
   clubs = db.session.scalars(db.select(Club)).all()
   if request.method == 'POST':
     name = request.form['name']
     position = request.form['position']
     nationality = request.form['nationality']
-    goals = int(request.form['goals'])
+    goal = int(request.form['goals'])
     squad_no = int(request.form['squad_no'])
     img = request.form['img']
     club_id = int(request.form['club_id'])
 
     player = Player(name=name, position=position, nationality=nationality,
-                    goals=goals, squad_no=squad_no, img=img, club_id=club_id)
+                    goal=goal, squad_no=squad_no, img=img, club_id=club_id)
+    
     db.session.add(player)
     db.session.commit()
+
     flash('add new player successfully', 'success')
     return redirect(url_for('players.index'))
-
+  
   return render_template('players/new_player.html',
                          title='New Player Page',
                          clubs=clubs)
 
-@player_bp.route('/search', methods=['POST'])
+@players_bp.route('/players/search', methods=['GET', 'POST'])
 def search_player():
   if request.method == 'POST':
     player_name = request.form['player_name']
@@ -43,23 +46,22 @@ def search_player():
                            title='Search Player Page',
                            players=players)
   
-@player_bp.route('/<int:id>/info')
+@players_bp.route('/<int:id>/info')
 def info_player(id):
   player = db.session.get(Player, id)
   return render_template('players/info_player.html',
                          title='Info Player Page',
                          player=player)
 
-@player_bp.route('/<int:id>/update', methods=['GET', 'POST'])
+@players_bp.route('/<int:id>/update', methods=['GET', 'POST'])
 def update_player(id):
   player = db.session.get(Player, id)
-  query = db.select(Club)
-  clubs = db.session.scalars(query).all()
+  clubs = db.session.scalars(db.select(Club)).all()
   if request.method == 'POST':
     name = request.form['name']
     position = request.form['position']
     nationality = request.form['nationality']
-    goals = int(request.form['goals'])
+    goal = int(request.form['goals'])
     squad_no = int(request.form['squad_no'])
     img = request.form['img']
     club_id = int(request.form['club_id'])
@@ -67,13 +69,14 @@ def update_player(id):
     player.name = name
     player.position = position
     player.nationality = nationality
-    player.goals = goals
+    player.goal = goal
     player.squad_no = squad_no
     player.img = img
     player.club_id = club_id
-
+    
     db.session.add(player)
     db.session.commit()
+
     flash('update player successfully', 'success')
     return redirect(url_for('players.index'))
   
